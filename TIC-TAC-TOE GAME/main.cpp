6cfc6c1f189766973b41e4dd5cdda4110b4f9e7b
Omar Ahmed El-Gazzar <92094226+OmmrGAZAR315@users.noreply.github.com>
2023-08-20 21:01:52 +0300

    Add files via upload

---
+++ TIC-TAC-TOE GAME/main.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+
+void printBoard(char element[3][3]);
+
+bool checkWin(char element[3][3]);
+
+void beginning(char element[3][3]);
+
+bool isItDraw(char element[3][3]);
+
+using namespace std;
+
+int main() {
+    char currentPlayer = 'X';
+    bool X_turn = true;
+    char elements[3][3] = {
+            {' ', ' ', ' '},
+            {' ', ' ', ' '},
+            {' ', ' ', ' '}
+    };
+    printBoard(elements);
+    while (true) {
+        X_turn = !X_turn;
+        int choice, row = 0, column;
+        cout << currentPlayer << " turn\n";
+        cin >> choice;
+        if (0 > choice || choice >= 10) break;
+        while (choice - 3 > 0) {
+            choice -= 3;
+            row++;
+        }
+        column = choice - 1;
+        if (elements[row][column] == ' ')
+            elements[row][column] = currentPlayer;
+        else cout << "already has been chosen\n";
+        if (checkWin(elements)) {
+            printBoard(elements);
+            cout << " Winner Winner chicken dinner\n\n";
+            cout << "wanna play again? (Y or N)  \n";
+            cin >> currentPlayer;
+            if (tolower(currentPlayer) == 'y') {
+                beginning(elements);
+                X_turn = true;
+            } else break;
+        }
+        if (X_turn)
+            currentPlayer = 'X';
+        else currentPlayer = 'O';
+        printBoard(elements);
+    }
+}
+
+void beginning(char elements[3][3]) {
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            elements[i][j] = ' ';
+        }
+    }
+}
+
+void printBoard(char element[3][3]) {
+    for (int r = 0; r < 3; r++) {
+        cout << "|";
+        for (int c = 0; c < 3; c++) {
+            cout << element[r][c] << " |";
+        }
+        cout << endl;
+        for (int u = 0; u < 3 && r != 2; u++) {
+            cout << "|--";
+        }
+        if (r != 2) cout << "|" << endl;
+    }
+
+}
+
+bool checkWin(char element[3][3]) {
+    for (int r = 0; r < 3; r++) {
+        if (element[r][0] == element[r][1] && element[r][1] == element[r][2] && element[r][0] != ' ') {
+            for (int i = 0; i < 3; i++)
+                element[r][i] = '-';
+            return true;
+        }
+    }
+    for (int c = 0; c < 3; c++) {
+        if (element[0][c] == element[1][c] && element[1][c] == element[2][c] && element[0][c] != ' ') {
+            for (int i = 0; i < 3; i++)
+                element[i][c] = '|';
+            return true;
+        }
+    }
+    if (element[0][0] == element[1][1] && element[1][1] == element[2][2] && element[0][0] != ' ') {
+        for (int i = 0; i < 3; ++i)
+            element[i][i] = '\\';
+        return true;
+    }
+    if (element[0][2] == element[1][1] && element[1][1] == element[2][0] && element[2][2] != ' ') {
+        element[0][2] = '/';
+        element[1][1] = '/';
+        element[2][0] = '/';
+        return true;
+    }
+    return false;
+}
